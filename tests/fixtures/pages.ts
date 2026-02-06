5187ba2d2b9abf3ed1d66f0d2b3fe1ba98ba87fd
ViktorTester <viktor345@inbox.lv>
2026-02-06 22:05:29 +0200

    build(env): added envConfig loading in pages.ts fixture

--- tests/fixtures/pages.ts
+++ tests/fixtures/pages.ts
@@ -1,6 +1,9 @@
+import {test as base, expect} from '@playwright/test'
 import {LandingPage} from "../../src/ui/pages/LandingPage";
 import {InputsPage} from "../../src/ui/pages/InputsPage";
-import { test as base, expect } from '@playwright/test'
+import {loadEnvConfig} from "../../src/utils/envLoader";
+import type {EnvConfig} from '../../src/types/EnvConfig';
+
 /**
  * Page Object fixtures
  *
@@ -10,19 +13,44 @@ import { test as base, expect } from '@playwright/test'
  */
 
 type PagesFixtures = {
+    config: EnvConfig;
     landing: LandingPage;
     inputs: InputsPage;
 }
 
-export const test = base.extend<PagesFixtures>(  {
-    landing: async ({ page }, use) => {
+export const test = base.extend<PagesFixtures>({
+
+    /**
+     * {scope: 'worker'}:
+     * “Load the configuration once for a group of tests
+     * executed within a single process, and reuse it across those tests.”
+     *
+     * env config is loaded once per worker
+     */
+    config: [
+        async (_args, use) => {
+            await use(loadEnvConfig());
+        }, {scope: 'worker'}
+    ],
+
+    // Page with baseURL applied
+    page: async ({browser, config}, use) => {
+        const context = await browser.newContext({
+            baseURL: config.baseUrl,
+        });
+
+        const page = await context.newPage();
+        await use(page);
+        await context.close();
+    },
+
+    landing: async ({page}, use) => {
         await use(new LandingPage(page));
     },
 
-    inputs: async ({ page }, use) => {
+    inputs: async ({page}, use) => {
         await use(new InputsPage(page))
     }
 })
-
 
 export {expect}
