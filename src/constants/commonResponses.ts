ad9e61e557aa69b20ad0bd2ea64fb70f7859c9a2
ViktorTester <viktor345@inbox.lv>
2026-02-28 22:08:44 +0200

    test(verify_login): added two tests

---
+++ src/constants/commonResponses.ts
@@ -0,0 +1,15 @@
+export const commonResponses = {
+    METHOD_NOT_SUPPORTED: {
+        code: 405,
+        message: 'This request method is not supported.'
+    },
+    BAD_REQUEST: {
+        code: 400,
+        message: 'Bad request.'
+    },
+    OK: {
+        code: 200,
+        message: 'OK'
+    }
+}
+
