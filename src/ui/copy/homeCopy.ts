9bc6d09497bbd31a5a06046337e43ca9a56da9a1
ViktorTester <viktor345@inbox.lv>
2026-04-10 23:07:38 +0300

    test(tc_22): added regression test

--- src/ui/copy/homeCopy.ts
+++ src/ui/copy/homeCopy.ts
@@ -1,5 +1,6 @@
 export const homeCopy = {
     subsriptionSuccess: "You have been successfully subscribed!",
-    productAddedToCart: "Your product has been added to cart."
+    productAddedToCart: "Your product has been added to cart.",
+    recommendedItems: "RECOMMENDED ITEMS"
 }
 
